70e5c41fc9e199d9196f95714219632977fb1cc8
AngeloChecked <angi99552@gmail.com>
2024-09-18 22:27:00 +0200

    add GA

--- components/Base.ts
+++ components/Base.ts
@@ -33,6 +33,7 @@ export const Base = (props: BaseProps) =>
       ${props.scripts ?? ""}
       ${props.style ?? ""}
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
+      ${googleTagManagerScript()}
     </head>
     <body>
       ${props.menu}
@@ -43,3 +44,15 @@ export const Base = (props: BaseProps) =>
     ${props.footer}
   </html>
 `;
+
+function googleTagManagerScript(){
+  return html`
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','G-LE27PDG685');</script>
+<!-- End Google Tag Manager -->
+`
+}
